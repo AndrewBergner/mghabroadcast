e65c69a534967bdcf97d6ecfff75587c0e4b36ab
andy <abergner@gmail.com>
2017-05-24 14:29:11 -0400

    Fixed bug in player iframe url.

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -1,2 +1,2 @@
 
-<iframe class="video-player" src="//iframe.dacast.com/b/85834/c/442018" frameborder="0" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+<iframe class="video-player" src="//iframe.dacast.com/b/92424/c/442018" frameborder="0" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
