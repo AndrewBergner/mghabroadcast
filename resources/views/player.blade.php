6f753d65bf832fb383d7a3658713726cb2ce227d
andy <abergner@gmail.com>
2017-07-05 09:03:08 -0400

    Updated player iframe

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -1,2 +1,2 @@
 
-<iframe class="video-player" src="//iframe.dacast.com/b/94390/c/444710" frameborder="0" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+<iframe class="video-player" src="//iframe.dacast.com/b/94732/c/445171" frameborder="0" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
